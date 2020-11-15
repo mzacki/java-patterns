bfaa34fb7c029d49636b370ad26652d0b8e201fb
mzacki <zacki.mateusz@gmail.com>
2020-11-15 20:06:28 +0100

    example of use

---
+++ src/main/java/edu/ant/patterns/builder/Main.java
@@ -0,0 +1,19 @@
+package edu.ant.patterns.builder;
+
+import edu.ant.solutions.logger.LoggingService;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Beehive myFirstBeehive = new Beehive.Builder()
+                .setBoxes(Collections.singletonList(new Box()))
+                .setColony(new Colony())
+                .setType(Type.OSTROWSKA)
+                .build();
+
+        LoggingService.logMessage("Automatically counted no. of boxes as height: " + myFirstBeehive.getHeight());
+        LoggingService.logMessage("My first beehive: " + myFirstBeehive.toString());
+    }
+
+}
