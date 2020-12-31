c23c52788485a7fa67a3af01a4d8bae6b8f99c22
mzacki <zacki.mateusz@gmail.com>
2020-12-31 12:29:21 +0100

    mongodb

---
+++ src/main/java/edu/ant/patterns/persistency/nosql/DocumentConverter.java
@@ -0,0 +1,16 @@
+package edu.ant.patterns.persistency.nosql;
+
+import edu.ant.patterns.io.repository.Alert;
+import org.bson.Document;
+
+public class DocumentConverter extends Document {
+
+    public static Document fromAlert(Alert alert) {
+        return new Document("_id", alert.getId())
+                .append("level", alert.getLevel())
+                .append("area", alert.getArea())
+                .append("type", alert.getType())
+                .append("probability", alert.getProbability());
+    }
+
+}
