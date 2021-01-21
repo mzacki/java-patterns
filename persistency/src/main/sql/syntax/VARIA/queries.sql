f502b5440ef136209f1ee78ea3f9603313e8492d
mzacki <zacki.mateusz@gmail.com>
2021-01-21 18:08:28 +0100

    deploy utils module to github mvn repo as library

--- persistency/src/main/sql/syntax/VARIA/queries.sql
+++ persistency/src/main/sql/syntax/VARIA/queries.sql
@@ -11,3 +11,28 @@ WHERE
         customer.last_name LIKE '%smith%'
   AND
         bc.to_date = '9999-01-01'
+
+-- count customers per current branch using view
+SELECT customer_branch_turnover_current.branch_name,
+       COUNT(*) AS customers_per_branch
+FROM
+    customer_branch_turnover_current -- view alias
+GROUP BY
+    branch_name
+
+-- count customers per field (including past fields)
+SELECT f.field_name,
+       COUNT(*) AS customers_per_field FROM
+    customer
+        INNER JOIN
+    field f on customer.customer_id = f.customer_id
+GROUP BY
+    f.field_name
+
+-- customers per turnover range rounded to multiplicity of 10000
+SELECT ROUND(customer_branch_turnover_current.turnover / 10000) AS range,
+       COUNT(*) AS customers_per_turnover
+FROM
+    customer_branch_turnover_current -- view alias
+GROUP BY
+    range
--- persistency/src/main/sql/syntax/VIEW/VIEW.sql
+++ persistency/src/main/sql/syntax/VIEW/VIEW.sql
@@ -9,7 +9,34 @@ SELECT company.*,
        c.registration_date
 FROM
     company
-        LEFT JOIN customer c ON company.customer_id = c.customer_id
+        LEFT JOIN customer c ON company.customer_id = c.customer_id;
 
 -- example of use
-SELECT * FROM company_left_join_customer -- view alias
+SELECT * FROM company_left_join_customer; -- view alias
+
+-- customer, current branch, current turnover
+CREATE VIEW customer_branch_turnover_current AS
+SELECT
+    customer.customer_id,
+    customer.first_name,
+    customer.last_name,
+    customer.registration_date,
+    b.branch_name,
+    bc.from_date AS branch_since,
+    t.turnover,
+    t.from_date AS monthly_turnover_since
+FROM
+    customer
+        INNER JOIN
+    branch_customers bc
+    ON customer.customer_id = bc.customer_id
+        INNER JOIN
+    branch b
+    ON bc.branch_id = b.branch_id
+        INNER JOIN
+    turnover t
+    ON customer.customer_id = t.customer_id
+WHERE
+        bc.to_date = '9999-01-01'
+  AND
+        t.to_date = '9999-01-01'
