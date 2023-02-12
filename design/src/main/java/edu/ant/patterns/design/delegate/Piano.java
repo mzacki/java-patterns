56fe229d3b0decb7959982bfc4efdf4198a8544d
mzacki <38253922+mzacki@users.noreply.github.com>
2023-02-12 16:24:22 +0100

    delegate

---
+++ design/src/main/java/edu/ant/patterns/design/delegate/Piano.java
@@ -0,0 +1,10 @@
+package edu.ant.patterns.design.delegate;
+
+public class Piano implements Instrument {
+
+    @Override
+    public void play() {
+        // plays piano
+    }
+
+}
---
+++ design/src/main/java/edu/ant/patterns/design/delegate/Violin.java
@@ -0,0 +1,9 @@
+package edu.ant.patterns.design.delegate;
+
+public class Violin implements Instrument{
+
+    @Override
+    public void play() {
+        // plays violin
+    }
+}
